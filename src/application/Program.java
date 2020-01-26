fcaf1ad60884a182bda2e6bc82fc4c78222bbf65
RaphaSilva625 <raphasilcosta@gmail.com>
2020-01-26 02:35:42 -0300

    improving treatment and adding the movement of the pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Scanner reader = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		
+		while(true){
 		UI.printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Source: ");
+		ChessPosition source = UI.readChessPosition(reader);
+		
+		System.out.println();
+		System.out.println("Target: ");
+		ChessPosition target = UI.readChessPosition(reader);
+		
+		ChessPiece capturedPiece = chessMatch.perfromChessMove(source, target);
+		}
 	}
 }
